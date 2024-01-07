eceb6c12bbdd6c350e003cb5b410c78d3ced26d3
kohsheen1234 <kohsheen.t@gmail.com>
2024-01-07 00:41:33 -0700

    Initial Commit

---
+++ OptionsPricing/OptionsPricingPriv.hpp
@@ -0,0 +1,17 @@
+//
+//  OptionsPricingPriv.hpp
+//  OptionsPricing
+//
+//  Created by Kohsheen Tiku on 1/7/24.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class OptionsPricingPriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
